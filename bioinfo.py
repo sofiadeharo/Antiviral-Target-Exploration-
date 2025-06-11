f938880e22f8955ce06092272c787bd9ff9a4001
Sofia De Haro <s.deharomacias@gmail.com>
2025-06-10 20:00:42 -0600

    New data, standarized values and prediction model

--- bioinfo.py
+++ bioinfo.py
@@ -1,17 +1,36 @@
 import pandas as pd
+import datetime as dt 
 from chembl_webresource_client.new_client import new_client# Target search for coronavirus
-#This program takes in a database with a certain virus, in this case, coronavirus, and classifies its drugs, compounds and effectivity. Then stores the new database to a csv document as preprocessed data. 
 import json
+from rdkit import DataStructs
+from rdkit.DataStructs.cDataStructs import ExplicitBitVect
+from rdkit.Chem import AllChem
 from rdkit import Chem
+
+#from rdkit.Chem import MorganGenerator
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+# Initialize the Morgan fingerprint generator
 from rdkit.Chem import Draw
+import numpy as np
+from rdkit.DataStructs import ConvertToNumpyArray
 
 
 
+organism_name= input("Enter the name of the organism you would like to look for: ")
+#This searches for our target 
 target = new_client.target
-target_query = target.search('coronavirus')
+target_query = target.search(organism_name)
+
+#convert to list of dictionaries 
+
+#Polars dataframe 
 targets = pd.DataFrame.from_dict(target_query)
-selected_target=targets.target_chembl_id.iloc[4]
-selected_target
+targets
+selected_target=targets.target_chembl_id[0]
 print(targets)
 
 
@@ -20,10 +39,15 @@ res=activity.filter(target_chembl_id=selected_target).filter(standard_type="IC50
 #print(res)
 
 df = pd.DataFrame.from_dict(res)
-df.head(3)
-df.standard_type.unique()
-df.to_csv('bioactivity_data.csv', index=False)
+df=df.merge(targets[['target_chembl_id','organism']],on='target_chembl_id', how='left')
 
+print(df)
+
+#
+#print(df.schema)
+
+df.to_csv('bioactivity_data.csv', index=False )
+print(df)
 
 df2 = df[df.standard_value.notna()]
 df2 = df2[df.canonical_smiles.notna()]
@@ -31,10 +55,17 @@ df2
 len(df2.canonical_smiles.unique())
 df2_nr = df2.drop_duplicates(['canonical_smiles'])
 df2_nr
+selection= ['organism','molecule_chembl_id', 'canonical_smiles','standard_value']
+df3=df2_nr[selection]
+df3.to_csv('bioactivity_data_preprocessed.csv', index=False )
+
+
+df4=pd.read_csv('bioactivity_data_preprocessed.csv')
+
 
 bioactivity_threshold = []
 
-for i in df2.standard_value:
+for i in df4.standard_value:
   if float(i) >= 10000:
     bioactivity_threshold.append("inactive")
   elif float(i) <= 1000:
@@ -48,28 +79,87 @@ for i in df2.molecule_chembl_id:
 mol_cid=[]
 for i in df2.molecule_chembl_id:
   mol_cid.append(i)
-
 canonical_smiles=[]
-for i in df2.canonical_smiles:
+for i in df4.canonical_smiles:
   canonical_smiles.append(i)
 
 standard_value=[]
-for i in df2.standard_value:
+for i in df4.standard_value:
   standard_value.append(i)
+  organism=[]
+  for i in df4.organism:
+    organism.append(i)
 
-data_tuples=list(zip(mol_cid,canonical_smiles,bioactivity_threshold,standard_value))
-df3=pd.DataFrame(data_tuples,columns=['molecule_chembl_id','canonical_smiles','bioactivity_threshold','standard_value'])
-print(bioactivity_threshold)
-df3
-df3.to_csv(r'processed_data.csv', index=False)
-print(df3)
+data_tuples=list(zip(organism,mol_cid,canonical_smiles,bioactivity_threshold,standard_value))
+df5=pd.DataFrame(data_tuples,columns=['organism','molecule_chembl_id','canonical_smiles','bioactivity_threshold','standard_value'])
+#print(bioactivity_threshold)
+df5
+df5.to_csv(r'processed_data.csv', index=False)
+print(df5)
 
-df= pd.read_csv("bioactivity_data.csv")
+#From now on the processes must be done based on the clean dataset
+df= pd.read_csv("processed_data.csv")
+df_data=pd.DataFrame(df)
+bioactivity_order=['active','intermediate','inactive'] #rearranging the organisms from active to inactive 
+df['bioactivity_threshold']=pd.Categorical(df['bioactivity_threshold'], categories=bioactivity_order,ordered=True)
+df_sorted=df.sort_values(by='bioactivity_threshold')
+print("Sorted Dataset")
+print(df_sorted)
 
+#Encoding Data
+#df_Encoded=pd.get_dummies(df, columns=['organism'])
+
+IC50_values=df_sorted['standard_value']
+print("Values")
+print(IC50_values)
+ic50_M=IC50_values/1e9
+df['PIC50']= -np.log10(ic50_M)
+print("IC50 + PIC50 Values")
+print(df)
+#print(df_Encoded)
+#morgan_gen = MorganGenerator(radius=2, fpSize=1024)
+df['mol'] = df['canonical_smiles'].apply(lambda smiles: Chem.MolFromSmiles(smiles))  # Convert SMILES to RDKit molecules
+df['ECFP4']=df['canonical_smiles'].apply(lambda smiles:  AllChem.GetMorganFingerprintAsBitVect(Chem.MolFromSmiles(smiles), radius=2, nBits=2048).ToBitString()) 
+fingerprints = np.array([list(AllChem.GetMorganFingerprintAsBitVect(mol, radius=2, nBits=2048)) for mol in df['mol']], dtype=int)
+np.save("fingerprints.npy",fingerprints)
+df.to_csv(r'processed_data1.csv', index=False)
+print(type(df['ECFP4'].iloc[0]))
+df=pd.read_csv("processed_data1.csv")
+df['ECFP4'] = df['ECFP4'].dropna().apply(lambda x: DataStructs.CreateFromBitString(str(x)))
+print(type(df['ECFP4'].iloc[0])) 
+print(df[['ECFP4']].head())
+
+
+
+#Defining variables x and y 
+x = np.vstack(df['ECFP4'].tolist())
+y=df['PIC50']
+print(x.shape, y.shape)
+
+
+#Testing variables 
+x_train, x_test, y_train, y_test=train_test_split(x,y, test_size=0.2,random_state=42)
+print("training set: ", x_train.shape, "Test set: ", x_test.shape)
+
+
+#Initializing model to test 
+model=RandomForestRegressor(n_estimators=100, random_state=42)
+model.fit(x_train, y_train)
+y_pred=model.predict(x_test)
+
+
+
+#Evaluating Performance: 
+print("MAE:", mean_absolute_error(y_test, y_pred))
+print("MSE:", mean_squared_error(y_test, y_pred))
+print("R² Score:", r2_score(y_test, y_pred))
+
+
+
+#Getting Image 
 df['mol']=df['canonical_smiles'].apply(Chem.MolFromSmiles)
-
-
 img=Draw.MolsToGridImage(df['mol'].head(13).tolist(),molsPerRow=13,subImgSize=(300,300))
 img.show()
+img
 img.save("output.png")
 
