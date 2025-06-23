49436a1d474bab07a3a95480bba76ffa70f623b8
Sofia De Haro <s.deharomacias@gmail.com>
2025-06-23 14:40:57 -0600

    Working code that WORKS

--- bioinfo.py
+++ bioinfo.py
@@ -6,7 +6,7 @@ from rdkit import DataStructs
 from rdkit.DataStructs.cDataStructs import ExplicitBitVect
 from rdkit.Chem import AllChem
 from rdkit import Chem
-
+import pubchempy as pcp
 #from rdkit.Chem import MorganGenerator
 
 from sklearn.model_selection import train_test_split
@@ -20,14 +20,11 @@ from rdkit.DataStructs import ConvertToNumpyArray
 
 
 
-organism_name= input("Enter the name of the organism you would like to look for: ")
+#organism_name= input("Enter the name of the organism you would like to look for: ")
+
 #This searches for our target 
 target = new_client.target
-target_query = target.search(organism_name)
-
-#convert to list of dictionaries 
-
-#Polars dataframe 
+target_query = target.search("e-coli")
 targets = pd.DataFrame.from_dict(target_query)
 targets
 selected_target=targets.target_chembl_id[0]
@@ -35,16 +32,17 @@ print(targets)
 
 
 activity=new_client.activity
-res=activity.filter(target_chembl_id=selected_target).filter(standard_type="IC50")#selects the ic50 values only 
+res=activity.filter(target_chembl_id=selected_target).filter(standard_type="IC50", assay_type="B", confidence_score_gte=7).only(
+    ['molecule_chembl_id', 'canonical_smiles', 'standard_value', 'standard_units', 'activity_comment']
+)
+#selects the ic50 values only 
 #print(res)
 
 df = pd.DataFrame.from_dict(res)
-df=df.merge(targets[['target_chembl_id','organism']],on='target_chembl_id', how='left')
-
+#df=df.merge(targets[['organism']],on='target_chembl_id', how='left')
+#'target_chembl_id'
 print(df)
 
-#
-#print(df.schema)
 
 df.to_csv('bioactivity_data.csv', index=False )
 print(df)
@@ -55,7 +53,7 @@ df2
 len(df2.canonical_smiles.unique())
 df2_nr = df2.drop_duplicates(['canonical_smiles'])
 df2_nr
-selection= ['organism','molecule_chembl_id', 'canonical_smiles','standard_value']
+selection= ['molecule_chembl_id', 'canonical_smiles','standard_value']
 df3=df2_nr[selection]
 df3.to_csv('bioactivity_data_preprocessed.csv', index=False )
 
@@ -86,12 +84,10 @@ for i in df4.canonical_smiles:
 standard_value=[]
 for i in df4.standard_value:
   standard_value.append(i)
-  organism=[]
-  for i in df4.organism:
-    organism.append(i)
+  
 
-data_tuples=list(zip(organism,mol_cid,canonical_smiles,bioactivity_threshold,standard_value))
-df5=pd.DataFrame(data_tuples,columns=['organism','molecule_chembl_id','canonical_smiles','bioactivity_threshold','standard_value'])
+data_tuples=list(zip(mol_cid,canonical_smiles,bioactivity_threshold,standard_value))
+df5=pd.DataFrame(data_tuples,columns=['molecule_chembl_id','canonical_smiles','bioactivity_threshold','standard_value'])
 #print(bioactivity_threshold)
 df5
 df5.to_csv(r'processed_data.csv', index=False)
@@ -106,8 +102,7 @@ df_sorted=df.sort_values(by='bioactivity_threshold')
 print("Sorted Dataset")
 print(df_sorted)
 
-#Encoding Data
-#df_Encoded=pd.get_dummies(df, columns=['organism'])
+#Converting the IC50 data to PIC50 for standarization
 
 IC50_values=df_sorted['standard_value']
 print("Values")
@@ -116,8 +111,9 @@ ic50_M=IC50_values/1e9
 df['PIC50']= -np.log10(ic50_M)
 print("IC50 + PIC50 Values")
 print(df)
-#print(df_Encoded)
-#morgan_gen = MorganGenerator(radius=2, fpSize=1024)
+
+
+#Getting Fingerprints from SMILES 
 df['mol'] = df['canonical_smiles'].apply(lambda smiles: Chem.MolFromSmiles(smiles))  # Convert SMILES to RDKit molecules
 df['ECFP4']=df['canonical_smiles'].apply(lambda smiles:  AllChem.GetMorganFingerprintAsBitVect(Chem.MolFromSmiles(smiles), radius=2, nBits=2048).ToBitString()) 
 fingerprints = np.array([list(AllChem.GetMorganFingerprintAsBitVect(mol, radius=2, nBits=2048)) for mol in df['mol']], dtype=int)
@@ -129,8 +125,17 @@ df['ECFP4'] = df['ECFP4'].dropna().apply(lambda x: DataStructs.CreateFromBitStri
 print(type(df['ECFP4'].iloc[0])) 
 print(df[['ECFP4']].head())
 
+#Labeling Rdkit images 
+#smiles=df['canonical_smiles']
+#compounds=pcp.get_compounds(smiles,namespace='smiles')
+#if compounds:
+ # print("Name: ",compounds[0].iupac_name or compounds[0].synonyms[0])
 
 
+
+
+
+#Model Training 
 #Defining variables x and y 
 x = np.vstack(df['ECFP4'].tolist())
 y=df['PIC50']
@@ -158,7 +163,8 @@ print("R² Score:", r2_score(y_test, y_pred))
 
 #Getting Image 
 df['mol']=df['canonical_smiles'].apply(Chem.MolFromSmiles)
-img=Draw.MolsToGridImage(df['mol'].head(13).tolist(),molsPerRow=13,subImgSize=(300,300))
+df['legend'] = df.apply(lambda row: f"{row['molecule_chembl_id']}\n{row['bioactivity_threshold'].capitalize()}| IC50:{row['standard_value']} µM", axis=1)
+img=Draw.MolsToGridImage(df['mol'].head(13).tolist(),molsPerRow=13,subImgSize=(300,300), legends=df['legend'].head(13).tolist())
 img.show()
 img
 img.save("output.png")
